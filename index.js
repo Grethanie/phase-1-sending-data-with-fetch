93bd2ca2428c0c096e4ed7a98942e59a45c67f60
Daniel Young <Danyoung91@gmail.com>
2022-11-22 22:18:29 -0500

    commit tree cfe0d9125cc54a4b8bcf2b8f39061a063701fea7

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Add your code here
+const submitData = (userNameString, userEmailString) => {
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      name: userNameString,
+      email: userEmailString,
+    }),
+  })
+    .then((response) => response.json())
+    .then((result) => {
+      const element = document.createElement("div");
+      element.innerHTML = result.id;
+      document.body.appendChild(element);
+    })
+    .catch(function (error) {
+      alert("Bad things! Ragnarők!");
+      const element = document.createElement("div");
+      element.innerHTML = error;
+      document.body.appendChild(element);
+    });
+};
+
+submitData("Grethanie", "danyoung91@gmail.com");
